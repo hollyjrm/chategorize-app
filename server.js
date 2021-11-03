3af80cd8393ae8b8c8fe62462fbf5d64d8fc2d69
Holly Morris <hollyjessicarosemorris@gmail.com>
2021-11-03 17:31:00 +0100

    basic message function

--- server.js
+++ server.js
@@ -11,8 +11,12 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+
 io.on('connection', (socket) => {
     console.log('a user connected');
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
 });
 
 server.listen(5000, () => {
