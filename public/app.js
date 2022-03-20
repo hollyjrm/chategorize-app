29542097702c570db2444c2047dbc068995c2fd1
Holly Morris <hollyjessicarosemorris@gmail.com>
2022-03-20 11:39:36 +0100

    changed styling

--- public/app.js
+++ public/app.js
@@ -1,106 +1,100 @@
-var form = document.getElementById('form');
-var input = document.getElementById('input');
-let messages = document.getElementById('messages');
-let username = document.getElementById('yourname').textContent.trim();
-var messageHistoryCont = document.getElementById('history');
-var addFriendButton = document.getElementById('add-friend-form');
+var form = document.getElementById("form");
+var input = document.getElementById("input");
+let messages = document.getElementById("messages");
+let username = document.getElementById("yourname").textContent.trim();
+var messageHistoryCont = document.getElementById("history");
+var addFriendButton = document.getElementById("add-friend-form");
 var addName = document.getElementById("add-friend-name");
-var messageContainer = document.getElementById('message-container');
-let title = document.getElementById('chat-title');
-let thisId = document.getElementById('room-id');
+var messageContainer = document.getElementById("message-container");
+let title = document.getElementById("chat-title");
+let thisId = document.getElementById("room-id");
 let thisRoomId = thisId.textContent.trim();
 let chatnameTitle = title.textContent.trim();
-console.log(`title man: ${chatnameTitle}`)
-let chatMembers = document.getElementById('chat-members')
+let chatMembers = document.getElementById("chat-members");
 
 const socket = io();
 
-socket.emit('sendUserName', username);
+socket.emit("sendUserName", username);
 
 //join new room
 
 let obj = {
-    chatname: chatnameTitle,
-    id: thisRoomId
-}
+  chatname: chatnameTitle,
+  id: thisRoomId,
+};
 
-socket.emit('joinRoom', obj); // pass obj which contains title and id
+socket.emit("joinRoom", obj); // pass obj which contains title and id
 
-socket.on('chatMembers', function (currMembers) {
+socket.on("chatMembers", function (currMembers) {
+  console.log(currMembers);
+  let userSet = new Set(currMembers);
+  chatMembers.innerHTML = "";
 
-    console.log(currMembers)
-    let userSet = new Set(currMembers);
-    chatMembers.innerHTML = '';
+  userSet.forEach((el) => {
+    let each = document.createElement("div");
+    each.textContent = el.username;
+    chatMembers.appendChild(each);
+  });
 
-    userSet.forEach(el => {
-        let each = document.createElement('div');
-        each.textContent = el.username;
-        chatMembers.appendChild(each);
-    })
-
-    chatMembers.style.background = 'rgb(253, 252, 220)';
-})
+  chatMembers.style.background = "rgb(253, 252, 220)";
+});
 
 // displays message history from DB
-socket.on('outputMessage', function (msg) {
+socket.on("outputMessage", function (msg) {
+  if (msg.length && messageHistoryCont.innerHTML == "") {
+    for (var x = 0; x < msg.length; x++) {
+      var dbMessage = document.createElement("li");
 
-    if (msg.length && messageHistoryCont.innerHTML == "") {
-        for (var x = 0; x < msg.length; x++) {
-            var dbMessage = document.createElement('li');
-
-            dbMessage.classList.add("message-color");
-            if (msg[x].sender.username == username) {
-                dbMessage.id = "my-messages";
-            }
-            dbMessage.textContent = `${msg[x].sender.username}: ${msg[x].message}`;
-            var dateSent = document.createElement('div');
-            dateSent.textContent = msg[x].time;
-            dateSent.classList.add("small-font");
-            dbMessage.appendChild(dateSent);
-            messageHistoryCont.appendChild(dbMessage);
-        }
+      dbMessage.classList.add("message-color");
+      if (msg[x].sender.username == username) {
+        dbMessage.id = "my-messages";
+      }
+      dbMessage.textContent = `${msg[x].sender.username}: ${msg[x].message}`;
+      var dateSent = document.createElement("div");
+      dateSent.textContent = msg[x].time;
+      dateSent.classList.add("small-font");
+      dbMessage.appendChild(dateSent);
+      messageHistoryCont.appendChild(dbMessage);
     }
+  }
 });
 
-
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    if (input.value) {
-        socket.emit('chat message', input.value);
-        input.value = '';
-    }
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  if (input.value) {
+    socket.emit("chat message", input.value);
+    input.value = "";
+  }
 });
 
-addFriendButton.addEventListener('submit', function (e) {
-    e.preventDefault();
-    if (addName.value) {
-        socket.emit('addFriend', addName.value);
-        addName.value = '';
-        location.reload();
-        return false;
-    }
-})
-
-socket.on('chat message', function (msg) {
-    var item = document.createElement('li');
-
-    var dateSent = document.createElement('div');
-    dateSent.textContent = msg.time;
-    dateSent.classList.add("small-font");
-
-    item.classList.add("message-color");
-
-    if (msg.user == username) {
-        item.id = "my-messages";
-    }
-    item.textContent = `${msg.user}: ${msg.msg}`;
-    item.appendChild(dateSent);
-    messages.appendChild(item);
-    messageContainer.scrollTop = messageContainer.scrollHeight;
-
+addFriendButton.addEventListener("submit", function (e) {
+  e.preventDefault();
+  if (addName.value) {
+    socket.emit("addFriend", addName.value);
+    addName.value = "";
+    location.reload();
+    return false;
+  }
 });
 
-socket.on('userLeft', function (username) {
-    console.log(username + ' has left');
+socket.on("chat message", function (msg) {
+  var item = document.createElement("li");
 
-})
+  var dateSent = document.createElement("div");
+  dateSent.textContent = msg.time;
+  dateSent.classList.add("small-font");
+
+  item.classList.add("message-color");
+
+  if (msg.user == username) {
+    item.id = "my-messages";
+  }
+  item.textContent = `${msg.user}: ${msg.msg}`;
+  item.appendChild(dateSent);
+  messages.appendChild(item);
+  messageContainer.scrollTop = messageContainer.scrollHeight;
+});
+
+socket.on("userLeft", function (username) {
+  console.log(username + " has left");
+});
--- server.js
+++ server.js
@@ -1,37 +1,37 @@
 if (process.env.NODE_ENV !== "production") {
-    require('dotenv').config();
-    console.log('running in dev mode');
+  require("dotenv").config();
+  console.log("running in dev mode");
 }
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-const mongoose = require('mongoose');
-const Message = require('./models/message');
-const Room = require('./models/room');
-const path = require('path');
-const morgan = require('morgan');
-const session = require('express-session');
-const flash = require('connect-flash');
+const mongoose = require("mongoose");
+const Message = require("./models/message");
+const Room = require("./models/room");
+const path = require("path");
+const morgan = require("morgan");
+const session = require("express-session");
+const flash = require("connect-flash");
 
-const passport = require('passport');
-const LocalStrategy = require('passport-local');
-const User = require('./models/user');
-const mongoSanitize = require('express-mongo-sanitize');
-const catchAsync = require('./utils/catchAsync');
-const ExpressError = require('./utils/ExpressError');
-const { isLoggedIn } = require('./middleware');
-const moment = require('moment');
-const { v4: uuidv4 } = require('uuid');
-const userRoutes = require('./routes/user');
-const helmet = require('helmet');
-const nodemailer = require('nodemailer');
-const Email = require('email-templates');
-// const dbUrl = process.env.DB_URL
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/chatapp';
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
+const mongoSanitize = require("express-mongo-sanitize");
+const catchAsync = require("./utils/catchAsync");
+const ExpressError = require("./utils/ExpressError");
+const { isLoggedIn } = require("./middleware");
+const moment = require("moment");
+const { v4: uuidv4 } = require("uuid");
+const userRoutes = require("./routes/user");
+const helmet = require("helmet");
+const nodemailer = require("nodemailer");
+const Email = require("email-templates");
+const { SocketAddress } = require("net");
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/chatapp";
 const MongoDBStore = require("connect-mongo")(session);
 //array so I can identify all users and their sockets
 let users = [];
@@ -41,76 +41,71 @@ let users = [];
 let roomId;
 
 const transporter = nodemailer.createTransport({
-    // service: 'SendinBlue', 
-    host: 'smtp-relay.sendinblue.com',
-    port: 587,
-    auth: {
-        user: 'info@chategorize.com',
-        pass: process.env.PASS
-    }
+  // service: 'SendinBlue',
+  host: "smtp-relay.sendinblue.com",
+  port: 587,
+  auth: {
+    user: "info@chategorize.com",
+    pass: process.env.PASS,
+  },
 });
 
 const mail = new Email({
-    views: { root: './routes/templates', options: { extension: 'ejs' } },
-    message: {
-        from: 'Chategorize <info@chategorize.com>',
-
-    },
-    preview: false,
-    send: true,
-    transport: transporter
-
-
+  views: { root: "./routes/templates", options: { extension: "ejs" } },
+  message: {
+    from: "Chategorize <info@chategorize.com>",
+  },
+  preview: false,
+  send: true,
+  transport: transporter,
 });
-
-
 
 //connect to db
 // 'mongodb://localhost:27017/chatapp'
-mongoose.connect(dbUrl, {
-    useNewUrlParser: true
+mongoose
+  .connect(dbUrl, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("MONGO CONNECTION OPEN");
+  })
+  .catch((e) => {
+    console.log("MONGO CONNECTION ERROR");
+    console.log(e);
+  });
 
-})
-    .then(() => {
-        console.log("MONGO CONNECTION OPEN")
-    }).catch(e => {
-        console.log("OH NO MONGO ERROR")
-        console.log(e);
-    })
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(express.static('public/'));
-app.use(morgan('tiny'));
+app.use(express.static("public/"));
+app.use(morgan("tiny"));
 app.use(express.urlencoded({ extended: true }));
 app.use(mongoSanitize());
 
-const secret = process.env.SECRET || 'mischievemanaged';
+const secret = process.env.SECRET || "mischievemanaged";
 const store = new MongoDBStore({
-    url: dbUrl,
-    secret,
-    touchAfter: 24 * 60 * 60
-
+  url: dbUrl,
+  secret,
+  touchAfter: 24 * 60 * 60,
 });
 
 store.on("error", function (e) {
-    console.log("SESSION STORE ERROR", e)
-})
+  console.log("SESSION STORE ERROR", e);
+});
 const sessionConfig = {
-    store,
-    name: 'session',
-    secret,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        httpOnly: true,
-        // secure: true,
-        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-        maxAge: 1000 * 60 * 60 * 24 * 7
-    }
-}
-app.use(session(sessionConfig))
+  store,
+  name: "session",
+  secret,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    // secure: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
+app.use(session(sessionConfig));
 app.use(flash());
 app.use(helmet({ contentSecurityPolicy: false }));
 
@@ -118,39 +113,45 @@ app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 
-passport.serializeUser(User.serializeUser())
+passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
-    res.locals.currentUser = req.user;
-    res.locals.success = req.flash('success');
-    res.locals.error = req.flash('error');
-    next();
-})
+  res.locals.currentUser = req.user;
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
+});
 
-app.use('/', userRoutes);
+app.use("/", userRoutes);
 
 /////////////////////////////////////////////////////////routes//////////////////////////////////////////////////////
 
-app.get('/', (req, res) => {
-    res.render(__dirname + '/views/index.ejs');
+app.get("/", (req, res) => {
+  res.render(__dirname + "/views/index.ejs");
 });
 
-app.get('/chat', isLoggedIn, async (req, res) => {
-    const chats = await Room.find({ users: req.user._id });
-    res.render(__dirname + '/views/chat.ejs', { currUser: req.user, chats });
+app.get("/chat", isLoggedIn, async (req, res) => {
+  const chats = await Room.find({ users: req.user._id });
+  res.render(__dirname + "/views/chat.ejs", { currUser: req.user, chats });
 });
 
-app.get('/new', isLoggedIn, (req, res) => {
-    res.render(__dirname + '/views/new.ejs', { currUser: req.user });
+app.get("/new", isLoggedIn, (req, res) => {
+  res.render(__dirname + "/views/new.ejs", { currUser: req.user });
 });
 
-app.post('/chatname', isLoggedIn, catchAsync(async (req, res) => {
-    if (!req.body.chatname) throw new ExpressError('No chatname provided', 400);
-    const person = await User.findOne({ username: req.user.username })
-    const newChat = new Room({ name: req.body.chatname, owner: person._id }, { autoIndex: false });
+app.post(
+  "/chatname",
+  isLoggedIn,
+  catchAsync(async (req, res) => {
+    if (!req.body.chatname) throw new ExpressError("No chatname provided", 400);
+    const person = await User.findOne({ username: req.user.username });
+    const newChat = new Room(
+      { name: req.body.chatname, owner: person._id },
+      { autoIndex: false }
+    );
     if (!newChat.users.includes(person._id)) {
-        newChat.users.push(person);
+      newChat.users.push(person);
     }
 
     await newChat.save();
@@ -158,148 +159,161 @@ app.post('/chatname', isLoggedIn, catchAsync(async (req, res) => {
 
     // redirect here to the newly made chatroom
     res.redirect(`/chatname/${newChat._id}`);
-}));
+  })
+);
 
-app.get('/chatname/:id', isLoggedIn, catchAsync(async (req, res) => {
+app.get(
+  "/chatname/:id",
+  isLoggedIn,
+  catchAsync(async (req, res) => {
     const users = await User.find({});
     const { id } = req.params;
 
     const room = await Room.findById(id);
-    res.render(__dirname + '/views/chatname.ejs', { currUser: req.user, users, room });
-}));
-
-app.all('*', (req, res, next) => {
-    next(new ExpressError('Page Not Found', 404))
-})
-
-app.use((err, req, res, next) => {
-    const { statusCode = 500 } = err;
-    if (!err.message) err.message = "Oh No, Something Went Wrong...:("
-    res.status(statusCode).render('error', { err })
-})
-
-
-/////////////////////////////////////////////////socket.io starts here//////////////////////////////////////////////////
-io.on('connection', (socket) => {
-    console.log("SOCKET CONNECTION")
-    socket.on('sendUserName', function (username) {
-        socket.username = username;
-        users.push({ name: socket.username, id: socket.id });
-        console.log(users);
-
+    res.render(__dirname + "/views/chatname.ejs", {
+      currUser: req.user,
+      users,
+      room,
     });
+  })
+);
 
-    let wantedRoom;
-
-    socket.on('joinRoom', async function ({ chatname, id }) {
-        wantedRoom = await Room.findById(id); // roomId is undefined
-
-        if (!wantedRoom) {
-            //if room doesn't exist: rooms should be created in new route only
-            console.log(`sorry ${chatname} does not exist`)
-        }
-        else {
-            const person = await User.findOne({ username: socket.username });
-            console.log(`if room DOES exist this is room: ${wantedRoom}`)
-
-            // if user isn't in the room already
-            if (!wantedRoom.users.includes(person._id)) {
-                wantedRoom.users.push(person)
-                wantedRoom.save();
-            }
-        }
-
-        let wantedRoomId = id;
-        let currMembers;
-
-        Room.findById(wantedRoomId).populate({ path: 'users', select: 'username -_id' }).then((result) => {
-
-            // all people in the room
-            currMembers = result.users;
-            io.in(wantedRoomId).emit('chatMembers', currMembers);
-
-        })
-        // retrieve message history from db
-        Message.find({ room: wantedRoomId }).populate('sender').then((result) => {
-            io.in(wantedRoomId).emit('outputMessage', result)
-        })
-
-        socket.join(wantedRoomId);
-
-        socket.on('addFriend', async function (person) {
-            try {
-                const user = await User.findOne({ username: person });
-
-                if (!wantedRoom.users.includes(user._id)) {
-                    wantedRoom.users.push(user)
-                    wantedRoom.save();
-
-                }
-            }
-
-            catch (e) {
-                console.log(`ADD FRIEND ERROR`, e);
-            }
-        })
-
-        socket.on('chat message', async function (msg) {
-            const messageSender = await User.findOne({ username: socket.username })
-            const message = new Message({ message: msg, sender: messageSender, time: moment().format('LLLL'), room: wantedRoom });
-            message.save().then(() => {
-                const toSend = { user: socket.username, msg: msg, time: moment().format('LLLL') }
-                io.in(wantedRoomId).emit('chat message', toSend);
-            })
-
-            let currEmails = [];
-
-            Room.findById(wantedRoomId).populate({ path: 'users', select: 'email -_id' }).then((result) => {
-
-                // all people in the room
-
-                console.log(`result is: ${Object.values(result.users)}`)
-                console.log('blah' + result.users[0].email)
-                for (let i = 0; i < result.users.length; i++) {
-                    currEmails.push(result.users[i].email)
-                    console.log(currEmails)
-                }
-                console.log('curr is ' + currEmails);
-                console.log(`message sender is: ${messageSender.email}`)
-                const index = currEmails.indexOf(messageSender.email);
-                if (index > -1) {
-                    currEmails.splice(index, 1);
-                }
-
-                mail.send({
-                    template: 'newMessage',
-                    message: {
-                        to: currEmails
-                    },
-                    locals: {
-
-                        roomTitle: wantedRoom,
-                        sender: messageSender.username
-
-                    }
-                })
-                    .then(console.log)
-                    .catch(console.error);
-
-            })
-
-        });
-    })
-
-    //only triggers when socket disconnects
-    socket.on('disconnect', function () {
-        var connectionMessage = socket.username + " Disconnected from Socket " + socket.id;
-        console.log(connectionMessage);
-        socket.broadcast.emit('userLeft', socket.username);
-    });
+app.all("*", (req, res, next) => {
+  next(new ExpressError("Page Not Found", 404));
 });
 
+app.use((err, req, res, next) => {
+  const { statusCode = 500 } = err;
+  if (!err.message) err.message = "Oh No, Something Went Wrong...:(";
+  res.status(statusCode).render("error", { err });
+});
+
+/////////////////////////////////////////////////socket.io starts here//////////////////////////////////////////////////
+io.on("connection", (socket) => {
+  console.log("SOCKET CONNECTION");
+  socket.on("sendUserName", function (username) {
+    socket.username = username;
+    users.push({ name: socket.username, id: socket.id });
+    console.log(users);
+  });
+
+  let wantedRoom;
+
+  socket.on("joinRoom", async function ({ chatname, id }) {
+    wantedRoom = await Room.findById(id); // roomId is undefined
+
+    if (!wantedRoom) {
+      //if room doesn't exist: rooms should be created in new route only
+      console.log(`sorry ${chatname} does not exist`);
+    } else {
+      const person = await User.findOne({ username: socket.username });
+      console.log(`if room DOES exist this is room: ${wantedRoom}`);
+
+      // if user isn't in the room already
+      if (!wantedRoom.users.includes(person._id)) {
+        wantedRoom.users.push(person);
+        wantedRoom.save();
+      }
+    }
+
+    let wantedRoomId = id;
+    let currMembers;
+
+    Room.findById(wantedRoomId)
+      .populate({ path: "users", select: "username -_id" })
+      .then((result) => {
+        // all people in the room
+        currMembers = result.users;
+        io.in(wantedRoomId).emit("chatMembers", currMembers);
+      });
+    // retrieve message history from db
+    Message.find({ room: wantedRoomId })
+      .populate("sender")
+      .then((result) => {
+        io.in(wantedRoomId).emit("outputMessage", result);
+      });
+
+    socket.join(wantedRoomId);
+
+    socket.on("addFriend", async function (person) {
+      try {
+        const user = await User.findOne({ username: person });
+
+        if (!wantedRoom.users.includes(user._id)) {
+          wantedRoom.users.push(user);
+          wantedRoom.save();
+        }
+      } catch (e) {
+        console.log(`ADD FRIEND ERROR`, e);
+      }
+    });
+
+    socket.on("chat message", async function (msg) {
+      const messageSender = await User.findOne({ username: socket.username });
+      const message = new Message({
+        message: msg,
+        sender: messageSender,
+        time: moment().format("LLLL"),
+        room: wantedRoom,
+      });
+      message.save().then(() => {
+        const toSend = {
+          user: socket.username,
+          msg: msg,
+          time: moment().format("LLLL"),
+        };
+        io.in(wantedRoomId).emit("chat message", toSend);
+      });
+
+      let currEmails = [];
+
+      Room.findById(wantedRoomId)
+        .populate({ path: "users", select: "email -_id" })
+        .then((result) => {
+          // all people in the room
+
+          console.log(`result is: ${Object.values(result.users)}`);
+          console.log("email is" + result.users[0].email);
+          for (let i = 0; i < result.users.length; i++) {
+            currEmails.push(result.users[i].email);
+            console.log(currEmails);
+          }
+          console.log("curr is " + currEmails);
+          console.log(`message sender is: ${messageSender.email}`);
+          const index = currEmails.indexOf(messageSender.email);
+          if (index > -1) {
+            currEmails.splice(index, 1);
+          }
+
+          mail
+            .send({
+              template: "newMessage",
+              message: {
+                to: currEmails,
+              },
+              locals: {
+                roomTitle: wantedRoom,
+                sender: messageSender.username,
+              },
+            })
+            .then(console.log)
+            .catch(console.error);
+        });
+    });
+  });
+
+  //only triggers when socket disconnects
+  socket.on("disconnect", function () {
+    let connectionMessage =
+      socket.username + " Disconnected from Socket " + socket.id;
+    console.log(connectionMessage);
+
+    socket.broadcast.emit("userLeft", socket.username);
+  });
+});
 
 // running server
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
-    console.log(`listening on port ${port}`);
+  console.log(`listening on port ${port}`);
 });
